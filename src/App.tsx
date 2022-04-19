79f910afe23bce66a5cb29b4583d382c43aa7fd0
rycont <rycont@outlook.kr>
2022-04-19 20:00:02 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ReactComponent as Favicon } from "./favicon.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <Favicon fill="red" />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import svgrPlugin from "vite-plugin-svgr";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgrPlugin({
+      svgrOptions: {},
+    }),
+  ],
+});
